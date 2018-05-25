247384c7cdb61626db03fa8b0f51c27c486f2974
Iftekhar1458 <tituchowdhuryict@gmail.com>
2018-05-25 16:47:05 +0600

    second

--- app/src/main/java/com/example/seru/a25052018project/MainActivity.java
+++ app/src/main/java/com/example/seru/a25052018project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.seru.a25052018project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void press(View view)
+    {
+        Toast.makeText(MainActivity.this, "button has been preesed", Toast.LENGTH_SHORT).show();
     }
 }
